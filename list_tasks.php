48cffcc371d00870594fa6eeeef5dad3a9fe89d2
Pierre du Plessis <pierre@pcservice.co.za>
2018-02-13 15:02:23 +0200

    Update flow of list_tasks.php to make logical sense

--- list_tasks.php
+++ list_tasks.php
@@ -1,23 +1,22 @@
 <?php
 
-$taskData = json_decode(file_get_contents('Task_Data.json'));
+$taskData = json_decode(file_get_contents('Task_Data.json'), true);
 
-$html = '<a id="newTask" href="#" class="list-group-item" data-toggle="modal" data-target="#myModal">
+$html = '';
+
+if ($taskData) {
+    foreach ($taskData as $task) {
+        $html .= '<a id="'.$task['TaskId'].'" href="#" class="list-group-item" data-toggle="modal" data-target="#myModal">
+                    <h4 class="list-group-item-heading">'.$task['TaskName'].'</h4>
+                    <p class="list-group-item-text">'.$task['TaskDescription'].'</p>
+                </a>';
+    }
+} else {
+    $html = '<a id="newTask" href="#" class="list-group-item" data-toggle="modal" data-target="#myModal">
                     <h4 class="list-group-item-heading">No Tasks Available</h4>
                     <p class="list-group-item-text">Click here to create one</p>
                 </a>';
-
-if (!$taskData) {
-    die($html);
 }
 
-if (count($taskData) > 0) {
-    $html = '';
-    foreach ($taskData as $task) {
-        $html .= '<a id="'.$task->TaskId.'" href="#" class="list-group-item" data-toggle="modal" data-target="#myModal">
-                    <h4 class="list-group-item-heading">'.$task->TaskName.'</h4>
-                    <p class="list-group-item-text">'.$task->TaskDescription.'</p>
-                </a>';
-    }
-}
-die($html);
+echo $html;
+
